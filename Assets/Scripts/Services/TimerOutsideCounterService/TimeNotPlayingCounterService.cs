0d66c77c85c01a56ddfc5b8e8c2231635f78bfb7
Sergio CM <sergiocmdev@gmail.com>
2022-10-21 19:12:05 +0200

    Añadido servicio para actualizar el tiempo estando fuera del juego

---
+++ Assets/Scripts/Services/TimerOutsideCounterService/TimeNotPlayingCounterService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+namespace Services.TimerOutsideCounterService
+{
+    //We can add interface in case we need another implementation
+    [CreateAssetMenu(fileName = "TimeNotPlayingCounterService", menuName = "Loadable/Services/TimeNotPlayingCounterService")]
+    public class TimeNotPlayingCounterService : LoadableComponent
+    {
+        // private ILoader _loader;
+        // private ISaver _saver;
+        private DateTime lastTime;
+
+        public void ExitingApp()
+        {
+            lastTime = DateTime.Now;
+            //Save
+        }
+
+        public int GetMinutesOutside()
+        {
+            //Load lastTime
+            var difference = DateTime.Now - lastTime;
+            int minutesSinceLastExiting = difference.Days * 24 * 60 +
+                                          difference.Hours * 60 +
+                                          difference.Minutes;
+            return minutesSinceLastExiting;
+        }
+
+        public override void Execute()
+        {
+            Debug.Log("[TimeNotPlayingCounterService] Iniciamos inicializacion");
+            //Get loader and saver
+        }
+    }
+}
